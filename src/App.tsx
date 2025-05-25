0d5556426d19ce3ce2a20a0767920266c7e1add0
ariasbruno <163929731+ariasbruno@users.noreply.github.com>
2025-05-25 14:31:13 -0300

    fix 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import NotFound from "./pages/404";
 
 const App = () => (
       <BrowserRouter>
